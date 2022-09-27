ea81074942c807b6f99c0a7a5944cded2dd3b2c8
Ramish Hassan <ramishshah93@gmail.com>
2022-09-28 00:31:26 +0200

    added file todo.go in root for CRUD functions

---
+++ todo.go
@@ -0,0 +1,39 @@
+package todo
+
+import (
+	"errors"
+	"time"
+)
+
+type item struct {
+	Task string
+	Done bool
+	CreatedAt time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item
+
+func (t *Todos) add(task string) {
+
+	todo:= item{
+		Task: task,
+		Done: false,
+		CreatedAt: time.Now(),
+		CompletedAt: time.Time{},
+	}
+	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("invaild number to todo to mark complete")
+	}
+
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
+}
+
