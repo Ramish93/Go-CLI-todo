be378f1f7be67f0f9025bdc1712aba282b59f31b
Ramish Hassan <ramishshah93@gmail.com>
2022-09-28 00:37:08 +0200

    added CRUD funcitonality in todo.go file

--- todo.go
+++ todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -33,6 +36,38 @@ func (t *Todos) Complete(index int) error {
 
 	ls[index-1].CompletedAt = time.Now()
 	ls[index-1].Done = true
+
+	return nil
+}
+
+func (t *Todos) Delete(index int) error {
+	ls:= *t
+	if index <= 0 || index > len(ls){
+		return errors.New("")
+	}
+	*t = append(ls[:index-1], ls[index:]...)
+
+	return nil
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist){
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return err
+	}
+
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
