5fe1b078530ecdd59d8b715a054031bfb8f7009a
Ramish Hassan <ramishshah93@gmail.com>
2022-09-30 10:56:51 +0200

    deleting the list,showing the function/switch statment complete

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	todo "github.com/Ramish93/Go-CLI-todo"
+)
+
+const todoFile = ".todos.json"
+
 
 func main() {
-	fmt.Println("hello world")
+	add := flag.Bool("add",false,"add a new todo")
+	complete := flag.Int("complete", 0, "mark a  todo as completed")
+	del := flag.Int("del", 0, "Delete a todo")
+	list:= flag.Bool("list", false, "list all todos")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+	
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add(task: "sample todo")
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Println(os.stderr, err.Error())
+			os.Exit(1)
+		}
+	case complete > 0:
+		err:= todos.Complete(*complete)
+		if err != nil {
+			fmt.Println(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *del > 0 :
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Println(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err:= todos.Store(todoFile)
+		if err != nil {
+			fmt.Println(os.stderr, err.Error())
+			os.Exit(1)
+		}
+	case *list:
+		todos.Print()
+	default:
+		fmt.Println(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
+}
+
+func(t *Todos) Print(){
+
+	for i,item := range *t {
+		fmt.Printf(format: "%d: %s\n", i, item.Task)
+	}
 }
