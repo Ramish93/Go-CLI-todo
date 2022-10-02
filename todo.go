4cd2925b7ec6d8eb46030160d3b53bd19b569d22
Ramish Hassan <ramishshah93@gmail.com>
2022-10-03 00:01:04 +0200

    building table cells and text in them in table

--- todo.go
+++ todo.go
@@ -3,9 +3,12 @@ package todo
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"time"
+
+	"github.com/alexeyco/simpletable"
 )
 
 type item struct {
@@ -84,5 +87,28 @@ func (t *Todos) Store(filename string) error {
 
 func(t *Todos) Print(){
 
-	
+	table := simpletable.New()
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done?"},
+			{Align: simpletable.AlignRight, Text: "CreatedAt"},
+			{Align: simpletable.AlignRight, Text: "CompletedAt"},
+		},
+	}
+	var cells [][]*simpletable.Cell
+
+	for idx, item := range *t{
+		idx ++
+		cells = append(cells, *&[]*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", idx)},
+			{Text: item.Task},
+			{Text: fmt.Sprintf("%t", item.Done)},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: item.CompletedAt.Format(time.RFC822)},
+			
+		})
+	}
+	table.Body = &simpletable.Body{Cells: cells}
 }
